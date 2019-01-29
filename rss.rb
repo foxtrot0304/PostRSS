94983337e2ac56cb1b80a77814c09a8156585b6e
foxtrot0304 <git@lifeissunnyday.com>
2019-01-30 01:13:38 +0900

    get list of blogs from file

--- rss.rb
+++ rss.rb
@@ -4,12 +4,13 @@ require 'rss'
 require 'feed-normalizer'
 require 'open-uri'
 
-blogs = [
-  "http://foxtrot0304.hatenablog.com/rss",
-]
 
-blogs.each do |url|
-  atom = FeedNormalizer::FeedNormalizer.parse(open(url))
+def load_blogs
+  File.read("blog.txt")
+end
+
+load_blogs.each_line do |url|
+  atom = FeedNormalizer::FeedNormalizer.parse(open(url.chomp))
   puts atom.entries.first.title
   puts atom.entries.first.url
   puts atom.last_updated
