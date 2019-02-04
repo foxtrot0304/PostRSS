51316166d670564091a650d977f89f493012a77f
foxtrot0304 <git@lifeissunnyday.com>
2019-02-05 01:31:03 +0900

    push link of blogs

--- web_integration_blogs.rb
+++ web_integration_blogs.rb
@@ -14,31 +14,22 @@ def load_blogs
   File.read("blog.txt")
 end
 
-def get_new_article
-load_blogs.each_line do |url|
-  atom = FeedNormalizer::FeedNormalizer.parse(open(url.chomp))
-  puts atom.entries.first.title
-  puts atom.entries.first.url
-  puts atom.last_updated
-end
-end
-
 def load_url
   YAML.load_file("url.yaml")
 end
 
-def load_post_param(title,url)
+def load_post_param(title,url,last_updated)
 post = <<"EOS"
 {
   "attachments":[
     {
       "fallback":"fallback",
-      "pretext":"attachments",
-      "color":"#D00001",
+      "pretext":"#{last_updated}",
+      "color":"#7CFC00",
       "fields":[
         {
-          "title":"hello",
-          "value":"world"
+          "title":"#{title}",
+          "value":"#{url}"
         }
       ]
     }
@@ -59,9 +50,7 @@ req["Content-Type"] = "application/json"
 
 load_blogs.each_line do |url|
   atom = FeedNormalizer::FeedNormalizer.parse(open(url.chomp))
-  req.body = load_post_param(atom.entries.first.title,atom.entries.first.url)
+  req.body = load_post_param(atom.entries.first.title,atom.entries.first.url,atom.last_updated)
   req["Content-Length"] = req.body.length.to_s
-  p req.body
   result = http.request(req)
-  p result
 end
